e739d4374f9776c0fa598eae2c6467a680b8b786
御风 <54386576+hongyingwei@users.noreply.github.com>
2024-05-20 13:33:34 +0800

    cacheDemo

---
+++ cacheDemo/doc/cache.sql
@@ -0,0 +1,69 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 5.7.44-log : Database - ehcache
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ehcache` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ehcache`;
+
+/*Table structure for table `areas` */
+
+DROP TABLE IF EXISTS `areas`;
+
+CREATE TABLE `areas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `areaid` varchar(20) NOT NULL,
+  `area` varchar(50) NOT NULL,
+  `cityid` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3306 DEFAULT CHARSET=utf8 COMMENT='行政区域县区信息表';
+
+/*Data for the table `areas` */
+
+insert  into `areas`(`id`,`areaid`,`area`,`cityid`) values (1,'110101','东城区','110100'),(3303,'110102','西城区','110100'),(3304,'110103','北城区','110101'),(3305,'110104','南城区','110101');
+
+/*Table structure for table `cities` */
+
+DROP TABLE IF EXISTS `cities`;
+
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cityid` varchar(20) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `provinceid` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=355 DEFAULT CHARSET=utf8 COMMENT='行政区域地州市信息表';
+
+/*Data for the table `cities` */
+
+insert  into `cities`(`id`,`cityid`,`city`,`provinceid`) values (1,'110100','市辖区','110000'),(354,'110101','县辖区','110000');
+
+/*Table structure for table `provinces` */
+
+DROP TABLE IF EXISTS `provinces`;
+
+CREATE TABLE `provinces` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `provinceid` varchar(20) NOT NULL,
+  `province` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=utf8 COMMENT='省份信息表';
+
+/*Data for the table `provinces` */
+
+insert  into `provinces`(`id`,`provinceid`,`province`) values (1,'110000','北京市');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
